bea4884a626f7fe6d3cec0039332cf33f5315726
firewood <karamaki@gmail.com>
2017-03-05 02:14:30 +0900

    add resources

---
+++ MyWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyWebViewController.h
+//  turbo_webview
+//
+//  Created by Kenichi Aramaki on 2017/03/04.
+//  Copyright © 2017年 Pepsilover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyWebViewController : UIViewController
+
+@end
